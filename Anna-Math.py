664855ddaaf1a32ddb453fa0b5ab8a4194f908ff
Mind15 <2mind@mail.ru>
2023-08-27 14:09:17 +0700

    Упростить генерацию примера

--- Anna-Math.py
+++ Anna-Math.py
@@ -1,6 +1,7 @@
+import msvcrt
 import random
 import time
-# import keyboard
+import keyboard
 
 
 def emulate_print_cat(delay=0.05):
@@ -23,18 +24,17 @@ def check_input(user_input):
     else:
         return False
 
+
 def main():
     # Зададим количество решаемых примеров
-    amount = 3
+    amount1 = 10
 
     start_time = time.time()
     amount_right_answers = 0
     amount_wrong_answers = 0
-    while amount_right_answers < amount:
-        first_num = random.randint(1,9)
-        second_num = random.randint(0,9)
-        num1 = first_num * 10 + second_num
-        third_num = random.randint(3,9)
+    while amount_right_answers < amount1:
+        num1 = random.randint(11,99)
+        num2 = random.randint(3,9)
 
         sign = random.randint(0,1)
         if sign:
@@ -46,8 +46,10 @@ def main():
 
         # ждем ввод и проверяем, что введено
         Stat1 = True
+
         while Stat1:
-            print(f'{num1} {arithmetic_operator} {third_num} = ', end='')
+            print(f'{num1} {arithmetic_operator} {num2} = ', end='', flush=True)
+
             input_item2 = input()
             if check_input(input_item2):
                 input_item = int(input_item2)
@@ -55,15 +57,15 @@ def main():
             else:
                 print('Вводи цифры, а не символы!')
 
-        if input_item == num1 + third_num * k:
+        if input_item == num1 + num2 * k:
             amount_right_answers += 1
-            print(f"Всего правильных ответов {amount_right_answers} из {amount}\n")
+            print(f"Всего правильных ответов {amount_right_answers} из {amount1}\n")
         else:
-            print("Ошибка! Правильный ответ =", num1 + third_num * k)
+            print("Ошибка! Правильный ответ =", num1 + num2 * k)
             amount_wrong_answers += 1
             if  amount_right_answers > 0:
                 amount_right_answers -= 1
-            print(f"Всего правильных ответов {amount_right_answers} из {amount}\n")
+            print(f"Всего правильных ответов {amount_right_answers} из {amount1}\n")
 
 
     end_time = time.time()
